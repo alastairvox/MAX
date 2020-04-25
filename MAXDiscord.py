47f73b7ed4e97c14ce47d16be49c4729dee5de90
DrawnActor <22815637+DrawnActor@users.noreply.github.com>
2020-04-25 14:19:32 -0600

    Finished configure, updated command architecture

--- MAXDiscord.py
+++ MAXDiscord.py
@@ -1,4 +1,4 @@
-import itertools, sys, traceback, asyncio
+import itertools, sys, traceback, asyncio, copy
 import discord, discord.ext.commands
 import MAXShared
 from MAXShared import query, authDB, configDB, dev
@@ -129,7 +129,7 @@ class MAXHelpCommand(discord.ext.commands.DefaultHelpCommand):
 description = "Ah, 'tis Max Headroom here, and I quote fro-fro-from the bard, Shakespeare, a writer:\n\n" \
             '*"I can performeth thine following commands:*'
 
-bot = discord.ext.commands.Bot(command_prefix=getGuildPrefix, case_insensitive=True, help_command=MAXHelpCommand(), description=description)
+bot = discord.ext.commands.Bot(command_prefix=getGuildPrefix, case_insensitive=True, help_command=MAXHelpCommand(), description=description, fetch_offline_members=True)
 
 
 # ---------- FUNCTIONS ----------
@@ -173,6 +173,75 @@ async def MAXMessageChannel(channel, role, message):
             await channel.send(role.mention + message)
             await role.edit(mentionable=False, reason="Sent message.")
 
+# add internal (boolean), originalCtx (Context), and userName (string)(only if not internal)
+# allows me to use converters easily from external commands and private messages
+# returns ctx, raises CheckFailure if no associated guild found
+async def modifyContext(ctx):
+    # if the context being passed in is from a discord event/called from a discord message then its an instance of the Context class from the discord.py API
+    internal = type(ctx) == discord.ext.commands.Context
+    # creates a shallow copy of the original context in case there are errors later
+    originalCtx = copy.copy(ctx)
+    if internal:
+        # manually change the context's guild to be the guild the user owns so we can just use the converters
+        ctx.guild = bot.get_guild(config.get(query.owner == ctx.author.id)['guildID']) if config.get(query.owner == ctx.author.id) else None
+        # add our stuff in so we can track if its internal or restore the orginal context for later
+        ctx.internal = internal
+        ctx.originalCtx = originalCtx
+        if ctx.guild == None:
+            raise discord.ext.commands.CheckFailure(message="This command can only be used by a discord server owner.")
+        return ctx
+    else:
+        userName = ctx
+        ctx = await createFakeContext()
+        for entry in config:
+            if userName in entry['ownerNames']:
+                ctx.guild = bot.get_guild(entry['guildID'])
+                break
+        ctx.internal = internal
+        ctx.originalCtx = originalCtx
+        ctx.userName = userName
+        # if no guild was associated with the external user
+        if ctx.guild == None:
+            raise discord.ext.commands.CheckFailure(message="This command can only be used by a discord server owner.")
+        return ctx
+
+def configNewGuilds():
+    for guild in bot.guilds:
+        if not config.get(query.guildID == guild.id):
+            print('Found new guild:', '"' + guild.name + '"', '(' + str(guild.id) + ').', 'Setting defaults...')
+            # the @everyone role everyone starts with, so we can find the first channel on the server everyone can send messages in (probably general) - we have to do this because default channels got removed in 2017 and theres no way to know what channel is the "general" channel anymore :/
+            defaultRole = guild.default_role
+            defaultRolePerms = defaultRole.permissions
+            defaultChannel = None
+            for channel in guild.text_channels:
+                channelRoleOverwrites = channel.overwrites_for(defaultRole)
+                # if the role has "send_messages" permission, then we are looking for the first channel that doesn't specifically deny that permission (overwrites.send_messages != False)
+                if defaultRolePerms.send_messages == True:
+                    if channelRoleOverwrites.send_messages != False:
+                        defaultChannel = channel
+                        break
+                # if the role doesn't have "send_messages" permission, then we are looking for the first channel that specifically allows that permission
+                else:
+                    if channelRoleOverwrites.send_messages == True:
+                        defaultChannel = channel
+                        break
+            # if we couldn't find a channel everyone can speak in, then fall back to finding the first channel our bot can speak in (should be the very first channel as an admin, not ideal but necessary for a default config)
+            if not defaultChannel:
+                for channel in guild.text_channels:
+                    channelPermissions = channel.permissions_for(bot.user)
+                    if channelPermissions.send_messages == True:
+                        defaultChannel = channel
+                        break
+            # if there are no channels we can speak in at all somehow then we just use a dm channel with the guild owner
+            if not defaultChannel:
+                if guild.owner.dm_channel:
+                    defaultChannel = guild.owner.dm_channel
+                else:
+                    guild.owner.create_dm()
+                    defaultChannel = guild.owner.dm_channel
+            defaultConfig = {'guildID': guild.id, 'owner': guild.owner.id, 'ownerNames': [], 'botChannel': defaultChannel.id, 'announceChannel': defaultChannel.id, 'streamRole': 'everyone', 'useActivity': False, 'deleteAnnouncements': True, 'badInternet': False}
+            config.upsert(defaultConfig, query.guildID == guild.id)
+
 
 # ---------- EVENTS ----------
 #
@@ -183,22 +252,30 @@ async def MAXMessageChannel(channel, role, message):
 
 @bot.event
 async def on_ready():
-    print(f'Connected as {bot.user}.')
+    print(f'Connected as {bot.user} to "' + '", "'.join(map(str, bot.guilds)) + '"')
     # get all the guilds MAX is connected to and check if they all have entries in the config file, if not then create a default one
+    configNewGuilds()
+
+@bot.event
+async def on_guild_join(guild):
+    configNewGuilds()
 
 @bot.event
 async def on_command(ctx):
-    print('Command', '"'+ ctx.invoked_with +'"','invoked by', ctx.author.name + '#' + ctx.author.discriminator, '('+ str(ctx.author.id) +')', 'on server "' + str(ctx.guild) + '".' if not ctx.guild == None else 'in private message.')
+    print('Command', '"'+ ctx.invoked_with +'"','invoked by', str(ctx.author), '('+ str(ctx.author.id) +')', 'on server "' + str(ctx.guild) + '".' if not ctx.guild == None else 'in private message.')
 
 @bot.event
 async def on_command_error(ctx, error):
     """The event triggered when an error is raised while invoking a command.
     ctx   : Context
     error : Exception"""
-    
-    # check if internal or external command
-    if ctx.message.id == 0:
-        print('SHITS FAKE YO')
+
+    if hasattr(ctx, 'internal'):
+        if not ctx.internal:
+            print("This an external command, my guy. We gotta raise another error or something maybe? Somehow pass this shit back.")
+            return
+    if hasattr(ctx, 'originalCtx'):
+        ctx = ctx.originalCtx
 
     # This prevents any commands with local handlers being handled here in on_command_error.
     if hasattr(ctx.command, 'on_error'):
@@ -219,12 +296,12 @@ async def on_command_error(ctx, error):
 
     elif isinstance(error, discord.ext.commands.NoPrivateMessage):
         try:
-            return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+            return await ctx.author.send(f'{ctx.command} can not be used in private messages.')
         except:
             pass
-    elif isinstance(error, discord.ext.commands.UserInputError):
-        print('Error: ' + str(error))
-        errorResponse = '**Error:** ' + str(error) + ' Use ``' + ctx.prefix + 'help ' + ctx.command.name + '`` for more information on this command.```' + ctx.prefix + ctx.command.name + ' ' + ctx.command.signature + '```'
+    elif isinstance(error, discord.ext.commands.UserInputError) or isinstance(error, discord.ext.commands.CheckFailure):
+        print(type(error).__name__ + ' Error: ' + str(error))
+        errorResponse = '**' + type(error).__name__ + ' Error:** *' + str(error) + '*\nSome arguments are case-sensitive. Use ``' + ctx.prefix + 'help ' + ctx.command.name + '`` for more information on this command.```' + ctx.prefix + ctx.command.name + ' ' + ctx.command.signature + '```'
         return await ctx.send(errorResponse)
 
     # # For this error example we check to see where it came from...
@@ -244,6 +321,57 @@ async def on_command_error(ctx, error):
 # ---------- COMMANDS ----------
 
 
+@bot.command(name='configure', help="""Check or change MAX options like announcement channel, command prefix, stream role, etc.
+
+    **option:**
+        The option you want to check or change (not case-sensitive):
+            ``prefix`` - the prefix (``!``, ``.``, ``-``, etc) that you want MAX to use for commands on your server.
+            ``owner`` - the user (name or ID) that you want to be allowed to configure MAX or access sensitive commands.
+            ``botChannel`` - the channel (name or ID) you want MAX to tell people to use commands in (when making announcements, etc).
+            ``announceChannel`` - the channel (name or ID) you want MAX to make announcements in.
+            ``streamRole`` - the role (name or ID) that you want MAX to @ mention for announcements and give to users when they join the server (can also be ``everyone``, ``here``, or ``none``). Since MAX keeps roles internally by ID, you don't have to use this if you have only changed the name of your previous stream role: just when you want to change it to a different role object that already exists.
+    
+    **value:**
+        The new value for the specified option (case-sensitive). Don't include this in your command if you just want to check what it's currently set to.""")
+async def configure(ctx, option, value=None):
+    ctx = await modifyContext(ctx)
+
+    option = option.lower()    
+    if option == 'prefix':
+        converter = None
+    elif option == 'owner':
+        converter = discord.ext.commands.UserConverter()
+    elif option == 'botchannel':
+        option = 'botChannel'
+        converter = discord.ext.commands.TextChannelConverter()
+    elif option == 'announcechannel':
+        option = 'announceChannel'
+        converter = discord.ext.commands.TextChannelConverter()
+    elif option == 'streamrole':
+        option = 'streamRole'
+        converter = discord.ext.commands.RoleConverter()
+    else:
+        raise discord.ext.commands.BadArgument(message="An invalid option was provided.")
+    
+    if value == None:
+        value = str(config.get(query.guildID == ctx.guild.id)[option])
+        if converter:
+            result = option + ' for your server "'+ ctx.guild.name +'" is: ``' + str(await converter.convert(ctx, value)) + ' (ID: ' + value + ')' + "``\nI hope that makes y-y-you happier than it makes me."
+        else:
+            result = option + ' for your server "'+ ctx.guild.name +'" is: ``' + value + "``\nI hope that makes y-y-you happier than it makes me."
+        if ctx.internal:
+            await ctx.send(result)
+        return
+    
+    converted = await converter.convert(ctx, value)
+    valueID = converted.id if converter else value
+
+    config.update({option: valueID}, query.guildID == ctx.guild.id)
+    result = option + ' for your server "'+ ctx.guild.name +'" has been set to: ``' + str(value) + "``\nMaybe these guys will listen to w-w-what you have to say now? Just kidding, who cares? I'm sorry. I-I-I care."
+    if ctx.internal:
+        await ctx.send(result)
+
+
 @bot.command(name='ping', help='Just responds with "pong!" to test if MAX is alive or not.')
 async def ping(ctx):
     await ctx.send('pong!')
@@ -251,54 +379,26 @@ async def ping(ctx):
 @bot.command(name='say', rest_is_raw=True, help="""Allows you to speak through MAX on your server. Use it in a DM with MAX so that others don't see your command!
 
     **channel:**
-        The name of the channel you want the message sent to (general, announcements, etc).
+        The name of the channel you want the message sent to (``general``, ``announcements``, etc).
     
     **notifyRole:**
-        The role you want notified of the message. Type the name of any role on your server and MAX will try to @ mention it (you can use a case-sensitive role name or a role ID). You can also enter here, everyone, streamrole (to mention the role you configured for your stream announcements), or none (for no @ mention).
+        The role you want notified of the message. Type the name of any role on your server and MAX will try to @ mention it (you can use a case-sensitive role name or a role ID). You can also enter ``here``, ``everyone``, ``streamrole`` (to mention the role you configured for your stream announcements), or ``none`` (for no @ mention).
     
     **text:**
         The message you want to send.""")
 async def say(ctx, channel, notifyRole, *, message):
-    # if the context being passed in is from a discord event/called from a discord message then its an instance of the Context class from the discord.py API
-    internal = type(ctx) == discord.ext.commands.Context
+    ctx = await modifyContext(ctx)
     
-    # uses an actual context and changes its guild if called from discord, otherwise creates a fake context and sets the guild if called form another service (ctx is username)
-    async def setContextGuild(ctx):
-        if internal:
-            # manually change the context's guild to be the guild the user owns so we can just use the converters
-            ctx.guild = bot.get_guild(config.get(query.owner == ctx.author.id)['guildID']) if config.get(query.owner == ctx.author.id) else None
-            return ctx
-        else:
-            userName = ctx
-            ctx = await createFakeContext()
-            for entry in config:
-                if userName in entry['ownerNames']:
-                    ctx.guild = bot.get_guild(entry['guildID'])
-                    break
-            return ctx
-
-    # save the original context in case there are errors to yell about?
-    originalContext = ctx
-
-    # change the guild to the one we want to check so we can use converters
-    ctx = await setContextGuild(ctx)
-    
-    # if there was no guild associated with the author/username
-    if not ctx.guild:
-        return False
-
     converter = discord.ext.commands.TextChannelConverter()
     channel = await converter.convert(ctx, channel)
 
     if notifyRole.lower() == 'streamrole':
         notifyRole = ctx.guild.get_role(config.get(query.guildID == ctx.guild.id)['streamRole'])
-
     converter = discord.ext.commands.RoleConverter()
     notifyRole = await converter.convert(ctx, notifyRole) if not (notifyRole.lower() == 'none' or notifyRole.lower() == 'here' or notifyRole.lower() == 'everyone') else notifyRole
 
-    ctx = originalContext if internal else ctx
-
     await MAXMessageChannel(channel, notifyRole, message)
-    if internal:
-        await ctx.send("I have presented the unwashed masses with your g-g-glorious message. I can't promise they'll be ha-ha-happy about it, though.")
+    result = "I have presented the unwashed masses with your g-g-glorious message. I can't promise they'll be ha-ha-happy about it, though."
+    if ctx.internal:
+        await ctx.send(result)
     return True
